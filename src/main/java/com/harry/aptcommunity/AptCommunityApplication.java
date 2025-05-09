c5a45f544890aa14c402b6400b7c9e330f22610e
함호철 <hamhochul@hamhocheol-ui-MacBookPro.local>
2025-05-09 15:29:32 +0900

    init: first commit

---
+++ src/main/java/com/harry/aptcommunity/AptCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.harry.aptcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptCommunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AptCommunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/harry/aptcommunity/AptCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.aptcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptCommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
